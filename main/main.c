b09e82cbf979c9323ee2b37c838b5a98b91fae29
Dawid K <dawka089@gmail.com>
2026-03-05 19:18:06 +0100

    demo: add periodic printout to observe base code

--- main/main.c
+++ main/main.c
@@ -1,9 +1,17 @@
+#include <stdio.h>
 
+#include "freertos/FreeRTOS.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+
+
+static const char *TAG = "iot-monitor-publisher";
 
 void app_main(void)
 {
-    while (1) 
-    {
-
+    uint32_t cnt = 0;
+    while (1) {
+        ESP_LOGI(TAG, "Infinity loop: %d", cnt++);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
